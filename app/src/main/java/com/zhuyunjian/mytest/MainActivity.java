787e8c6c6fd11fa65150e005fe83ed3c2eedb82b
zhu635433912 <1335112702@qq.com>
2016-03-01 10:19:53 +0800

    3.1修改2

--- app/src/main/java/com/zhuyunjian/mytest/MainActivity.java
+++ app/src/main/java/com/zhuyunjian/mytest/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
